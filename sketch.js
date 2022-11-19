06255b4e29bbe0f9117c305f4f6dcfb7e0167233
Kat Chudy <kec21@fsu.edu>
2022-11-19 17:44:42 -0500

    button attempt

--- sketch.js
+++ sketch.js
@@ -1,7 +1,19 @@
+let button;
+var on = false;
+
 function setup() {
   createCanvas(1000, 1000);
+  background(0);
+  button = createButton('GO TO THE DOCTOR');
+  button.position(100, 200);
+  button.mousePressed(button = createButton('YOUR DOCTOR HAS REFERRED YOU TO A SPECIALIST'));
+  button.position(300, 500);
+  button.mousePressed();
+  button.size(400,200)
+  button.size(200,100)
 }
-
 function draw() {
-  background(125);
+  background(220);
+
+
 }
